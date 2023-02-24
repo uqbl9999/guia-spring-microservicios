297a53c352cd733ea944d62a398d3c88df166c3e
Brandon <brandon.urtecho@unmsm.edu.pe>
2023-02-24 08:59:29 -0500

    Spring cloud gateway y resilience4j

---
+++ como-usar-resilience4j-con-spring-cloud-gateway/springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -0,0 +1,85 @@
+package com.formacionbdi.springboot.app.item.controllers;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import com.formacionbdi.springboot.app.item.models.Producto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.service.ItemService;
+
+@RestController
+public class ItemController {
+
+	private Logger logger = LoggerFactory.getLogger(ItemController.class);
+	@Autowired
+	private CircuitBreakerFactory cbFactory;
+	@Autowired
+	@Qualifier("serviceFeign")
+	private ItemService itemService;
+	
+	@GetMapping("/listar")
+	public List<Item> listar(
+			@RequestParam(name = "nombre", required = false) String nombre,
+			@RequestHeader(name = "token-request", required = false) String token
+	){
+		System.out.println("nombre = " + nombre);
+		System.out.println("token-request = " + token);
+		return itemService.findAll();
+	}
+
+	@GetMapping("/ver/{id}/cantidad/{cantidad}")
+	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return cbFactory.create("items")
+				.run(() -> itemService.findById(id, cantidad), e -> metodoAlternativo(id, cantidad, e));
+	}
+
+	@CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
+	@GetMapping("/ver2/{id}/cantidad/{cantidad}")
+	public Item detalle2(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return itemService.findById(id, cantidad);
+	}
+
+	@CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo2")
+	@TimeLimiter(name = "items")
+	@GetMapping("/ver3/{id}/cantidad/{cantidad}")
+	public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return CompletableFuture.supplyAsync(() -> itemService.findById(id, cantidad));
+	}
+
+	public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
+		logger.info(e.getMessage());
+		Item item = new Item();
+		Producto producto = new Producto();
+
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("camara sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+
+		return item;
+	}
+
+	public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e) {
+		logger.info(e.getMessage());
+		Item item = new Item();
+		Producto producto = new Producto();
+
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+
+		return CompletableFuture.supplyAsync(() -> item);
+	}
+}
