34bf3635ac4079dfba67c411857024d0135fd7e3
Brandon <brandon.urtecho@unmsm.edu.pe>
2023-02-25 10:55:00 -0500

    como usar spring cloud config server

---
+++ como-usar-spring-cloud-config-server/springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -0,0 +1,92 @@
+package com.formacionbdi.springboot.app.item.controllers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.Producto;
+import com.formacionbdi.springboot.app.item.models.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+// La anotación @RefreshScope de Spring se utiliza para habilitar
+// la actualización dinámica de las propiedades de configuración de una aplicación
+// en tiempo de ejecución. Esto significa que, si se realiza un cambio en la configuración
+// en el servidor de configuración y se envía una solicitud POST al punto final /actuator/refresh,
+// las instancias de los beans se actualizan automáticamente con las nuevas
+// propiedades de configuración sin necesidad de reiniciar la aplicación.
+// Por lo tanto, se recomienda utilizar la anotación @RefreshScope en los componentes
+// de la aplicación que dependen de las propiedades de configuración y que deben
+// actualizarse automáticamente en caso de que se realice un cambio en la configuración.
+@RefreshScope
+@RestController
+public class ItemController {
+	
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	@Qualifier("serviceFeign")
+	private ItemService itemService;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
+	
+	@GetMapping("/listar")
+	public List<Item> listar(){
+		return itemService.findAll();
+	}
+	
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
+	@GetMapping("/ver/{id}/cantidad/{cantidad}")
+	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return itemService.findById(id, cantidad);
+	}
+
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+		
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		return item;
+		
+	}
+
+	// Con este método se realiza la prueba del servidor de configuración por ejemplo comunicandose al
+	// http://localhost:8005/obtener-config
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+		
+		log.info(texto);
+		
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
+}
